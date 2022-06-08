df0ab5a472af9df10dcc2714b79511698165b0ba
Vo Thanh Vi <vothanhvi24.08@gmail.com>
2022-06-08 17:10:12 +0700

    fix bug

--- blog_website/blog_part/forms.py
+++ blog_website/blog_part/forms.py
@@ -1,6 +1,5 @@
 from unicodedata import category
 from django import forms
-from matplotlib.pyplot import title
 
 from user_profile.models import User
 
@@ -8,36 +7,36 @@ from .models import Blog, Category
 
 from ckeditor.fields import RichTextField
 
-from pathlib import Path
+# from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
+# BASE_DIR = Path(__file__).resolve().parent
 
-import pandas as pd
-try:
-    # if Category.objects.filter(title = 'Thế Giới').exists():
-    # if Category.objects.filter(title = 'Pháp Luật').exists():
-    if Category.objects.filter(title = 'Giáo Dục').exists():
-        pass
-    else:
-        # cate = Category(title = 'Thế Giới')
-        # cate = Category(title = 'Pháp Luật')
-        cate = Category(title = 'Giáo Dục')
-        cate.save()
+# import pandas as pd
+# try:
+#     # if Category.objects.filter(title = 'Thế Giới').exists():
+#     # if Category.objects.filter(title = 'Pháp Luật').exists():
+#     if Category.objects.filter(title = 'Giáo Dục').exists():
+#         pass
+#     else:
+#         # cate = Category(title = 'Thế Giới')
+#         # cate = Category(title = 'Pháp Luật')
+#         cate = Category(title = 'Giáo Dục')
+#         cate.save()
         
         
-    # df = pd.read_csv(BASE_DIR / 'TheGioi.csv')
-    df = pd.read_csv(BASE_DIR / 'GiaoDuc.csv')
-    for i in range(len(df)):
-        temp = Blog()
-        temp.user = User.objects.get(username = 'baotintuc')
-        # temp.category = Category.objects.get(title = 'Thế Giới')
-        temp.category = Category.objects.get(title = 'Giáo Dục')
-        temp.banner = df.iloc[i]['image']
-        temp.title = df.iloc[i]['title']
-        temp.description = df.iloc[i]['content']
-        temp.save()
-except:
-    pass
+#     # df = pd.read_csv(BASE_DIR / 'TheGioi.csv')
+#     df = pd.read_csv(BASE_DIR / 'GiaoDuc.csv')
+#     for i in range(len(df)):
+#         temp = Blog()
+#         temp.user = User.objects.get(username = 'baotintuc')
+#         # temp.category = Category.objects.get(title = 'Thế Giới')
+#         temp.category = Category.objects.get(title = 'Giáo Dục')
+#         temp.banner = df.iloc[i]['image']
+#         temp.title = df.iloc[i]['title']
+#         temp.description = df.iloc[i]['content']
+#         temp.save()
+# except:
+#     pass
 
 # obj = Blog.objects.get(id = 4)
 # obj.banner = 'media//blog_banner//thegioi1_6'
