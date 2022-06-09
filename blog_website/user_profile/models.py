d6da6bf62bbe96ba1f44e2e29937a2dc02d3f2cb
Vo Thanh Vi <vothanhvi24.08@gmail.com>
2022-06-09 11:08:07 +0700

    update-profile

--- blog_website/user_profile/models.py
+++ blog_website/user_profile/models.py
@@ -1,36 +1,52 @@
 from django.db import models
-import email
-import profile
 from django.contrib.auth.models import AbstractUser
 
 from .managers import CustomUserManager
-# Create your models here.
+
 
 class User(AbstractUser):
     email = models.EmailField(
-        max_length=100,
+        max_length=150,
         unique=True,
         error_messages={
             "unique": "The email must be unique"
         }
     )
-
     profile_image = models.ImageField(
-        null = True,
+        null=True,
         blank=True,
-        upload_to='profile_images'
+        upload_to="profile_images"
     )
+    followers = models.ManyToManyField("Follow")
 
-    REQUIRE_FIELDS = ['email']
-    object = CustomUserManager
+    REQUIRED_FIELDS = ["email"]
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.username
 
     def get_profile_picture(self):
-        url = ''
+        url = ""
         try:
             url = self.profile_image.url
         except:
-            url = ''
+            url = ""
         return url
+
+
+class Follow(models.Model):
+    followed = models.ForeignKey(
+        User,
+        related_name='user_followers',
+        on_delete=models.CASCADE
+    )
+    followed_by = models.ForeignKey(
+        User,
+        related_name='user_follows',
+        on_delete=models.CASCADE
+    )
+    muted = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.followed_by.username} started following {self.followed.username}"
